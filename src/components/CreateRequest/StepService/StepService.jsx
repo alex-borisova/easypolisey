c868884f47aa49dd46b390e304157c2e0debaf4f
alex-borisova <alex.pineapplle@gmail.com>
2022-11-16 22:38:13 +0200

    added fetch and get services

--- src/components/CreateRequest/StepService/StepService.jsx
+++ src/components/CreateRequest/StepService/StepService.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { apiUrl } from "../../../apiUrl";
 
 export const StepService = () => {
+  const [services, setServices] = useState([]);
+  console.log(services);
+
+  const getServices = async () => {
+    await fetch(`${apiUrl}/service_types`, {
+      method: "GET",
+    })
+      .then((response) =>
+        response.text().then((text) => {
+          return text && JSON.parse(text);
+        })
+      )
+      .then((res) => setServices(res.service_types));
+  };
+
+  useEffect(() => {
+    getServices();
+  }, []);
+
   return <>Lala</>;
 };
 
