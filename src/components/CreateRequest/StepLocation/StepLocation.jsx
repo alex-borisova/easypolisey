02a46c3694884fa0c59e73e48501e91fef0cc3ee
alex-borisova <alex.pineapplle@gmail.com>
2022-11-19 15:19:48 +0200

    change stepper and added step location

--- src/components/CreateRequest/StepLocation/StepLocation.jsx
+++ src/components/CreateRequest/StepLocation/StepLocation.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Box from "@mui/material/Box";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 export const StepLocation = () => {
-  return <>Papapa</>;
+  const [state, setState] = useState({
+    gilad: true,
+    jason: false,
+    antoine: false,
+  });
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const { gilad, jason, antoine } = state;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+        <FormLabel component="legend">
+          You are allowed to select multiple client service office, but only one
+          will be booked. Please select the appropriate Police and Border Guard
+          Board service office and click “Next”.
+        </FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox checked={gilad} onChange={handleChange} name="gilad" />
+            }
+            label="Gilad Gray"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={jason} onChange={handleChange} name="jason" />
+            }
+            label="Jason Killian"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={antoine}
+                onChange={handleChange}
+                name="antoine"
+              />
+            }
+            label="Antoine Llorca"
+          />
+        </FormGroup>
+      </FormControl>
+    </Box>
+  );
 };
 
 export default StepLocation;
