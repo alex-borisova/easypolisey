96944e72ee8319e85dc05a964067e156b69f473b
alex-borisova <alex.pineapplle@gmail.com>
2022-12-30 16:45:48 +0200

    fix button

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import EmailIcon from "@mui/icons-material/Email";
+
+import BottomNavigation from "@mui/material/BottomNavigation";
+
+export const Footer = () => {
+  const info = [
+    {
+      icon: <EmailIcon sx={{ color: "#fff" }} />,
+      text: "mail@example.com",
+    },
+    {
+      icon: <TelegramIcon sx={{ color: "#fff" }} />,
+      text: "@examplegroup",
+    },
+  ];
+  const styleInfo = {
+    display: "flex",
+    gap: 1,
+  };
+
+  const styleBottom = {
+    backgroundColor: "#1976d2",
+    justifyContent: "space-between",
+    alignItems: "center",
+    px: 3,
+    py: 1,
+  };
+
+  return (
+    <BottomNavigation sx={styleBottom}>
+      <Typography variant="body1" color="#fff">
+        &#169; 2022 Easypolisey
+      </Typography>
+      <Box display="flex" flexDirection="column">
+        {info.map((item, index) => (
+          <Box sx={styleInfo} key={index}>
+            {item.icon}
+            <Typography variant="body1" color="#fff">
+              {item.text}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+    </BottomNavigation>
+  );
+};
+
+export default Footer;
