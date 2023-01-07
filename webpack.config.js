fe091fc48378e8186cb670d6bd2da93d36d92ca5
alex-borisova <alex.pineapplle@gmail.com>
2023-01-07 22:29:22 +0200

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Test"
-        }),
+  entry: "./src/index.jsx",
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loaders: ["babel"],
+        include: path.join(__dirname, "src"),
+      },
     ],
-}
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Test",
+    }),
+  ],
+};
